b3e7e76e7fa049df3ed55517e3def2c7cc8f0876
JyuKa <yoyksks3@gmail.com>
2022-01-21 19:34:03 +0900

    Delete 구현 실습

---
+++ src/main/java/com/example/hello/controller/DeleteApiController.java
@@ -0,0 +1,20 @@
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteApiController {
+
+//    http://localhost:9090/api/delete/3?account=JyuKa
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+
+        System.out.println(userId);
+        System.out.println(account);
+
+
+        // delete -> 리소스 삭제 200 ok
+        // 삭제 했거나 이미 없기 때문에 항상 200
+    }
+}
--- src/main/java/com/example/hello/controller/PutApiController.java
+++ src/main/java/com/example/hello/controller/PutApiController.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class PutApiController {
 
-//    /api/put/3
-//{
-//    "name" : "jyuka",
-//        "age" : 13,
-//        "car_list" : [
-//    {"name" : "sonata", "car_number" : 123},
-//    {"name" : "sonata", "car_number" : 456}
-//   ]
-//}
+    //  http://localhost:9090/api/put/3
+
+    //{
+    //    "name" : "jyuka",
+    //        "age" : 13,
+    //        "car_list" : [
+    //    {"name" : "sonata", "car_number" : 123},
+    //    {"name" : "sonata", "car_number" : 456}
+    //   ]
+    //}
     @PutMapping("/put/{userId}")
     public PutRequestDto put(@RequestBody PutRequestDto requestDto, @PathVariable(name = "userId") Long id){
         System.out.println(requestDto);
