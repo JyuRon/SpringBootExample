e08c476a5b36e1e7bc649eb9958e6dd21f71840e
JyuKa <yoyksks3@gmail.com>
2022-01-26 17:38:43 +0900

    네이버 지역검색 API 활용 - 지역, 이미지 검색 service

--- src/test/java/com/example/hello/restaurant/naver/NaverClientTest.java
+++ src/test/java/com/example/hello/restaurant/naver/NaverClientTest.java
@@ -2,6 +2,7 @@ package com.example.hello.restaurant.naver;
 
 import com.example.hello.restaurant.naver.dto.SearchImageReq;
 import com.example.hello.restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,7 @@ class NaverClientTest {
 
         var result = naverClient.searchLocal(search);
         System.out.println(result);
+        Assertions.assertNotNull(result.getItems().stream().findFirst().get().getCategory());
     }
 
 
